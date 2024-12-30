952fbac688e5b59230d21d660bc01a0101501be9
Bjarke Mønsted <bjarkemonsted@gmail.com>
2024-12-30 21:37:14 +0100

    🚧 Changed structure
    Moved onedr... app in to apps folder, changed settings + more...

--- nostromo/apps/onedrivesharepoint/apps.py
+++ nostromo/apps/onedrivesharepoint/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OnedrivesharepointConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'onedrivesharepoint'
+    name = 'apps.onedrivesharepoint'
